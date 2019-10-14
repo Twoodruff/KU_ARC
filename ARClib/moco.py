f4d7e264425d20442be7c060959c73fa1d590508
Thomas <jazzyjayhawk@gmail.com>
2019-10-14 14:06:21 -0500

    Issue #17: added update() and cleaned documentation

--- ARClib/moco.py
+++ ARClib/moco.py
@@ -2,10 +2,8 @@
 File: moco.py
 Author: Thomas Woodruff
 Date: 8/07/19
-Revision: 0.1.1
-Description: Wrapper for actuators.py. Handles saturation condition,
-             converts input to writable signal and passes to actuator
-             control.
+Revision: 0.1
+Description: Handles all motion control for the car
 """
 
 import time
@@ -15,10 +13,18 @@ import adafruit_pca9685 as pca
 from adafruit_servokit import ServoKit
 import numpy as np
 
-#This class acts directly with the servo shield to control the servo and motor
+
 class __Actuate__:
+    '''
+    Class acts directly with the servo shield to control the servo and motor
+    '''
     def __init__(self, freq = 100):
-        """sets up I2C comms with the PCA9685 board"""
+        '''
+        Inputs:
+            freq : frequency for the I2C comms
+
+        Function: sets up I2C comms with the PCA9685 board
+        '''
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.hat = pca.PCA9685(self.i2c)
         self.kit = ServoKit(channels = 16)
@@ -29,7 +35,13 @@ class __Actuate__:
 
 
     def startup(self, mchannel, schannel):
-        'initializes the steering and motor to their neutral positions'
+        '''
+        Inputs:
+            mchannel : channel on PCA9685 connected to motor
+            schannel : channel on PCA9685 connected to servo
+
+        Function: initializes the steering and motor to their neutral positions
+        '''
         self.motorCh = mchannel
         self.steerCh = schannel
 
@@ -42,12 +54,22 @@ class __Actuate__:
 
 
     def Drive(self, throttle):
-        'applies the throttle value to the motor'
+        '''
+        Inputs:
+            throttle : PWM angle that represents the desired speed
+
+        Function: applies the throttle value to the motor
+        '''
         self.throttle = throttle
         self.kit.servo[self.motorCh].angle = throttle
 
     def Steer(self, direction):
-        'applies the direction value to the steering servo'
+        '''
+        Inputs:
+            direction : PWM angle that represents the desired steering angle
+
+        Function: applies the direction value to the steering servo
+        '''
         self.dir = direction
         self.kit.servo[self.steerCh].angle = self.dir
 
@@ -60,42 +82,59 @@ class __Actuate__:
 
 
     def shutdown(self):
-        'sets steering and motor back to their neutral positions'
+        '''
+        Function: stops car by setting steering and motor
+                  back to their neutral positions
+        '''
         self.kit.servo[self.motorCh].angle = 90
         self.kit.servo[self.steerCh].angle = 90
         print("System Shutdown...")
 
 
-#This class is an interface between the PCA board and the outside world
+
 class MotorController:
+    '''
+    Class is an interface between the PCA board and the outside world
+    Wrapper class for _Actuate_
+    '''
+    maxAngle = 135
+    minAngle = 45
+    maxSpeed = 115  #true max is higher
+    minSpeed = 65   #true min is lower
+
     def __init__(self):
-        'create a car instance and setup initial values'
+        '''
+        create an __Actuate__ instance and setup initial values
+        '''
         self.car = __Actuate__()
-        self.car.startup(3,0)                                                   #not a good place for this
+        self.car.startup(mchannel = 3, schannel = 0)
 
-        self.steer = self.car.getDir()                                          #use getters from actuators
+        self.steer = self.car.getDir()
         self.speed = self.car.getThrottle()
-
-        self.maxAngle = 135
-        self.minAngle = 45
-        self.maxSpeed = 115                                                     #true max is higher
-        self.minSpeed = 65                                                      #true min is lower
-
-        self.FWD = 1
-        self.BACK = -1
 
 
     def setSteer(self, angleIN):
-        'converts angleIN to readable value for __Actuate__'
-        'positive => right  negative => left'
+        '''
+        Inputs:
+            angleIN : desired steering angle
+                      positive => right / negative => left
+
+        Function: converts angleIN to readable value for __Actuate__
+        '''
         self.steer = angleIN + 90
         return float(self.steer)
 
 
     def setDrive(self, speedIN):
-        'maps speedIN from [-1,1] to angle value for __Actuate__'
-        slope = (self.maxSpeed-self.minSpeed)/2
-        self.speed = (speedIN-self.BACK)*slope + self.minSpeed                  #maps from input to angle
+        '''
+        Inputs:
+            speedIN : desired speed from [-1,1]
+                      pos => forward / neg => backward
+
+        Function: maps speedIN to angle value for __Actuate__
+        '''
+        slope = (MotorController.maxSpeed-MotorController.minSpeed)/2
+        self.speed = (speedIN-self.BACK)*slope + MotorController.minSpeed
         return float(self.speed)
 
     def getDrive(self):
@@ -105,89 +144,127 @@ class MotorController:
         return(self.steer)
 
     def run(self):
-        """
-        Checks saturation condition and writes to actuators
-        Called when state is updated
-        """
-        #steering condition
-        if (self.steer > self.maxAngle):
-            self.steer = self.maxAngle
-        elif (self.steer < self.minAngle):
-            self.steer = self.minAngle
+        '''
+        Function: Checks saturation condition and sets desired values
+        '''
+        # STEERING CONDITION
+        if (self.steer > MotorController.maxAngle):
+            self.steer = MotorController.maxAngle
+        elif (self.steer < MotorController.minAngle):
+            self.steer = MotorController.minAngle
+        else:
+            pass
 
+        # THROTTLE CONDITION
+        if (self.speed > MotorController.maxSpeed):
+            self.speed = MotorController.maxSpeed
+        elif (self.speed < MotorController.minSpeed):
+            self.speed = MotorController.minSpeed
+        else:
+            pass
+
+    def update(self):
+        '''
+        Function: writes to actuators
+        '''
         self.car.Steer(self.steer)
         #print("Steering: ", self.steer)
-
-        #throttle condition
-        if (self.speed > self.maxSpeed):
-            self.speed = self.maxSpeed
-        elif (self.speed < self.minSpeed):
-            self.speed = self.minSpeed
 
         self.car.Drive(self.speed)
         #print("Throttle: ", self.speed)
 
-
     def shutdown(self):
         self.car.shutdown()
-        #need a way to prevent more commands from being written after shutdown
 
 
-#This class is an open-loop control on the acceleration and steering sensitivity of a car
 class Accel:
+    '''
+    This class is an open-loop control on the acceleration and steering sensitivity of a car
+    '''
     def __init__(self, mc):
-        'define a MotorController object to act on'
+        '''
+        Inputs:
+            mc = MotorController object
+        '''
         self.mc = mc
 
 
     def rampSpd(self, start, stop=0):
-        """
-        controls the acceleration of the car
-        specify start and stop speed in range of [-1,1]
-        """
+        '''
+        Inputs:
+            start : start speed of accel/deccel in range [-1,1]
+            stop  : stop speed of accel/deccel in range [-1,1]
+
+        Function: controls the acceleration of the car
+        '''
+        # ACCELERATION
         if start>=stop:
-            for spd in np.arange(start, stop, -0.07):                           #step value chosen experimentally
+            for spd in np.arange(start, stop, -0.07):   #step value chosen experimentally
                 curr_spd = self.mc.setDrive(spd)
                 self.mc.run()
+                self.mc.update()
                 print(curr_spd)
-                time.sleep(0.45)                                                #delay value chosen experimentally
+                time.sleep(0.45)    #delay value chosen experimentally
+        # DECCELERATION
         elif start<stop:
             for spd in np.arange(start, stop, 0.07):
                 curr_spd = self.mc.setDrive(spd)
                 self.mc.run()
+                self.mc.update()
                 print(curr_spd)
                 time.sleep(0.45)
-        else:                                                                   #change to elif that checks 'stop' var
-            self.mc.shutdown()
+        # CONSTANT SPEED
+        elif start == stop:
+            curr_spd = self.mc.setDrive(start)
+            self.mc.run()
+            self.mc.update()
+            print(curr_spd)
+
+        else:
+            self.mc.stop()
 
 
     def rampDir(self, start, stop=0):
-        """
-        controls responsiveness of steering
-        """
-        if start>=0: #right
-            for dir in np.arange(start, stop, 2):                               #step value chosen experimentally
+        '''
+        Inputs:
+            start : start direction of turn in range [-1,1]
+            stop  : stop directin of turn in range [-1,1]
+
+        Function: controls responsiveness of steering
+        '''
+        # TURN RIGHT
+        if start>=0:
+            for dir in np.arange(start, stop, 2):   #step value chosen experimentally
                 curr_dir = self.mc.setSteer(dir)
                 self.mc.run()
+                self.mc.update()
                 print(curr_dir)
-                time.sleep(0.09)                                                #delay value chosen experimentally
-        elif start<0: #left
+                time.sleep(0.09)    #delay value chosen experimentally
+        # TURN LEFT
+        elif start<0:
             for dir in np.arange(start, stop, -2):
                 curr_dir = self.mc.setSteer(dir)
                 self.mc.run()
+                self.mc.update()
                 print(curr_dir)
                 time.sleep(0.09)
+        # NO TURN
+        elif start == stop:
+            curr_dir = self.mc.setSteer(start)
+            self.mc.run()
+            self.mc.update()
+            print(curr_dir)
+
         else:
-            self.mc.shutdown()                                                  #replace shutdown with stop condition
+            self.mc.stop()
 
 
     def stop(self):
+        '''
+        Function: stops car without shutting down
+        '''
         curr_spd = self.mc.setDrive(0)
         curr_dir = self.mc.setSteer(0)
         self.mc.run()
+        self.mc.update()
         print("Stop ", curr_spd, curr_dir)
-
-
-##    def setAccel(self, acc):                                                  #future setter that would allow for step and delay to chosen by user
-##                                                                              #...possibly
-##    def setResp(self, res):
