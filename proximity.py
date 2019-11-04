5cf8ea377c82f68a55ce221a97e4885b85a4fbd5
Ryan Strong <ryan.strongofficial@gmail.com>
2019-11-04 14:53:58 -0600

    proximity.py, adjusted indent

--- proximity.py
+++ proximity.py
@@ -7,7 +7,6 @@ Description: Ultrasonic sensor thread, used to measure distance by sending 8 40k
              time taken to receive a signal back. Using this time and speed of sound, distance is found.
              Module must include __init__, run, update, shutdown
 '''
-
 # IMPORTS
 import RPi.GPIO as GPIO
 import time
@@ -20,8 +19,7 @@ class distance:
    waves to return to the sensor. Knowing the speed of sound at sea level is 343 m/s we can
    find distance from sensor/car at any time
    '''
-
-   def __init__(self,inputs):
+    def __init__(self,inputs):
         self.TRIG=23
         self.ECHO=24
         GPIO.setup(self.TRIG,GPIO.OUT)
@@ -29,29 +27,28 @@ class distance:
         GPIO.output(self.TRIG,False)
         time.sleep(.02)
        # INITIALIZE VARS
+        self.running = True
 
-       self.running = True
+    def run(self, inputs):
+        while self.running:
+            GPIO.output(self.TRIG,True)
+            time.sleep(.00001)
+            GPIO.output(self.TRIG,False)
 
-   def run(self, inputs):
-       while self.running:
-           GPIO.output(self.TRIG,True)
-           time.sleep(.00001)
-           GPIO.output(self.TRIG,False)
+            while GPIO.input(self.ECHO)==0:
+                pulse_start = time.time()
 
-           while GPIO.input(self.ECHO)==0:
-               pulse_start = time.time()
+            while GPIO.input(self.ECHO)==1:
+                pulse_end = time.time()
 
-           while GPIO.input(self.ECHO)==1:
-               pulse_end = time.time()
+            total_pulse_time = (pulse_end - pulse_start)*.5
+            distance = 343*total_pulse_time #distance is in meters
+            self.OUT = distance
 
-           total_pulse_time = (pulse_end - pulse_start)*.5
-           distance = 343*total_pulse_time #distance is in meters
-           self.OUT = distance
-
-   def update(self):
-       return self.OUT
+    def update(self):
+        return self.OUT
 
 
-   def shutdown(self):
-       GPIO.cleanup()
-       self.running = False
+    def shutdown(self):
+        GPIO.cleanup()
+        self.running = False
