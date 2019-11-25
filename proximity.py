737023f71ef77bc369891e20cc19f9947f65cd69
Twoodruff <jazzyjayhawk@gmail.com>
2019-11-25 09:35:04 -0600

    updates and fixes to proximity.py

--- proximity.py
+++ proximity.py
@@ -14,63 +14,70 @@ GPIO.setmode(GPIO.BCM)
 
 class Distance:
 
-   '''
-   HC-SR04 proximity sensor sends out ultrasonic waves and records time is takes for the
-   waves to return to the sensor. Knowing the speed of sound at sea level is 343 m/s we can
-   find distance from sensor/car at any time
-   '''
+    '''
+    HC-SR04 proximity sensor sends out ultrasonic waves and records time is takes for the
+    waves to return to the sensor. Knowing the speed of sound at sea level is 343 m/s we can
+    find distance from sensor/car at any time
+    '''
 
-   def __init__(self,trig,echo):
-        self.TRIG=trig
-        self.ECHO=echo
-        #self.OUT=0
+    def __init__(self):
+        self.TRIG=23
+        self.ECHO=24
         GPIO.setup(self.TRIG,GPIO.OUT)
         GPIO.setup(self.ECHO,GPIO.IN)
-        GPIO.setup(4,GPIO.OUT)
-        GPIO.setup(17,GPIO.OUT)
-        GPIO.output(17,False)
-        GPIO.output(4,True)
-
+        GPIO.output(self.TRIG, False)
+        time.sleep(2)
+        print("settling sensor")
         self.running = True
 
-   def run(self):
-       self.pulse_end=0
-       if self.running:
-           GPIO.output(self.TRIG,True)
-           time.sleep(.00001)
-           GPIO.output(self.TRIG,False)
-
-            while GPIO.input(self.ECHO)==0:
-                pulse_start = time.time()
-
-           while GPIO.input(self.ECHO)==1:
-               self.pulse_end = time.time()
+    def run(self):
+        if self.running:
+            new_reading = False
+            counter = 0
+            #send trigger pulse
+            GPIO.output(self.TRIG,True)
+            time.sleep(.00001)
+            GPIO.output(self.TRIG,False)
             
-           if self.pulse_end==0:
-               total_pulse_time = .0117
-           else:
-               total_pulse_time = (self.pulse_end - pulse_start)*.5
+            while GPIO.input(self.ECHO)==0:
+                pass
+                print("getting start time")
+                counter+=1
+                if counter>15:
+                    new_reading = True
+                    break
+            pulse_start = time.time()
         
-           self.OUT = 343*total_pulse_time #distance is in meters
+            if new_reading:
+                return False
+            
+            while GPIO.input(self.ECHO)==1:
+                pass
+                print("getting stop time")
+            pulse_end = time.time()
+
+            total_pulse_time = (pulse_end - pulse_start)*.5
+            self.OUT = 343*total_pulse_time #distance is in meters
                
-
     def update(self):
-        return self.OUT
+        if self.OUT<4:
+            return self.OUT
+        else:
+            return False
 
-
-   def shutdown(self):
-       self.running = False
-       GPIO.cleanup()
+    def shutdown(self):
+        self.running = False
+        GPIO.cleanup()
        
 if __name__ == "__main__":
-    testing_dis = Distance(trig=23,echo=24)
-    loop=0
+    testing_dis = Distance()
+    loop = 0
     try:
         while True:
-            testing_dis.run()
-            output=testing_dis.update()
+            output = testing_dis.run()
+            output = testing_dis.update()
             print("\nloop: ", loop)
-            print(output)
+            print("Distance:",output, "m")
             loop+=1
     except KeyboardInterrupt:
         testing_dis.shutdown()
