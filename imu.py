91d9fb53b60ff15346ed2cd88d3887453322edf3
Thomas <jazzyjayhawk@gmail.com>
2020-01-09 16:51:25 -0600

    fix merge markers

--- imu.py
+++ imu.py
@@ -1,12 +1,7 @@
 '''
 File: imu.py
-<<<<<<< HEAD
 Author: Thomas Woodruff
 Date: 1/9/2020
-=======
-Author: First Last
-Date: 1/8/2020
->>>>>>> more plotting, created imu.py class
 Revision: 0.1
 Description: Computes measurements of velocity and heading
              with IMU (myAHRS+).
@@ -18,20 +13,14 @@ import time
 import serial
 import traceback
 import math
-<<<<<<< HEAD
 import numpy as np
 import matplotlib.pyplot as plt
 
-=======
-import matplotlib.pyplot as plt
-import numpy as np
->>>>>>> more plotting, created imu.py class
 
 class IMU:
     '''
     description of class
     '''
-<<<<<<< HEAD
     def __init__(self,serial_device,output = 'rpy'): #add in default for RPI
         '''
         Inputs:
@@ -42,22 +31,10 @@ class IMU:
         # INITIALIZE PORT DEVICE
         try:
             self.serial_port = serial.Serial(serial_device, 115200, timeout=1.0)
-=======
-    def __init__(self,inputs):
-        '''
-        Inputs:
-            input1 :
-            input2 :
-        '''
-        # INITIALIZE VARS
-        try:
-            serial_port = serial.Serial(serial_device, 115200, timeout=1.0)
->>>>>>> more plotting, created imu.py class
         except serial.serialutil.SerialException:
             print('Can not open serial port(%s)'%(serial_device))
             traceback.print_exc()
 
-<<<<<<< HEAD
         # SET MODE AND OUTPUT TYPE
         self.send_command(self.serial_port, 'mode,AT')
 
@@ -77,14 +54,6 @@ class IMU:
 
 
     def run(self):
-=======
-        vel_x = 0
-        vel_y = 0
-        heading = yaw
-        self.running = True
-
-    def run(self, inputs):
->>>>>>> more plotting, created imu.py class
         '''
         Inputs: (optional)
             input1 : range/description
@@ -92,7 +61,6 @@ class IMU:
 
         Function: description
         '''
-<<<<<<< HEAD
 
         if self.running:
 
@@ -144,61 +112,12 @@ class IMU:
         '''
         return self.vel_x, self.vel_y, self.heading
 
-=======
-        #This method will be accessed by the Thread function
-        #Loops continually
-
-        while self.running:
-            last_loop = time.time()
-            last_write = last_loop
-
-            try:
-                # send trigger command
-                send_command(serial_port, 'trig')
-
-                # recieve data_message then parse it for data
-                items = parse_data_message_rpyimu(serial_port.readline().strip().decode())
-
-                if(items):
-                    sequence_number, roll, pitch, yaw, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, temperature = items
-
-                    accel_x, accel_y, accel_z = remove_gravity(accel_x, accel_y, accel_z, pitch, roll, yaw)
-
-                    current_loop = time.time()
-                    dt = current_loop - last_loop
-                    total_time = total_time + dt
-
-                    vel_x = vel_x + accel_x*dt
-                    vel_y = vel_y + accel_y*dt
-                    vel_z = vel_z + accel_z*9.81*dt
-
-                    last_loop = current_loop
-
-    def update(self,inputs):
-        '''
-        Inputs: (optional)
-            input1 : range/description
-            input2 : range/description
-
-        Function: description
-
-        Outputs:
-            output1 : range/description
-            output2 : range/description
-        '''
-        #This method will only be accessed once per drive cycle
-        #Return values should go here
-        return self.vars
->>>>>>> more plotting, created imu.py class
 
     def shutdown(self):
         #Clean up anything when done
         self.running = False
-<<<<<<< HEAD
         self.serial_port.close()
 
-=======
->>>>>>> more plotting, created imu.py class
 
     def send_command(self, serial_port, cmd_msg):
         '''
@@ -226,7 +145,6 @@ class IMU:
                 if(line[0] == '~'):
                     return line
 
-<<<<<<< HEAD
 
     def remove_gravity(self, accel_x, accel_y, accel_z, pitch, roll, yaw):
         '''
@@ -259,49 +177,3 @@ if __name__ == '__main__':
         print("x-velocity: {}\ny-velocity: {}\nheading: {}\n".format(vel_x, vel_y, heading))
         time.sleep(0.1)
         i += 1
-=======
-def parse_data_message_rpyimu(self, data_message):
-    '''
-    Inputs: (optional)
-        data_message : range/description
-
-    Function: description
-
-    Outputs:
-        RPY : range/description
-        Accel : range/description
-        Mag: range/description
-        Temp: range/description
-    '''
-    # $RPYIMU,39,0.42,-0.31,-26.51,-0.0049,-0.0038,-1.0103,-0.0101,0.0014,-0.4001,51.9000,26.7000,11.7000,41.5*1F
-
-    data_message = (data_message.split('*')[0]).strip() # discard crc field
-    fields = [x.strip() for x in data_message.split(',')]
-
-    if(fields[0] != '$RPYIMU'):
-        return None
-
-    sequence_number, roll, pitch, yaw, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, temperature = (float(x) for x in fields[1:])
-    return (int(sequence_number), roll, pitch, yaw, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, temperature)
-
-def remove_gravity(self, accel_x, accel_y, accel_z, pitch, roll, yaw):
-    '''
-    Inputs: (optional)
-        accel : acceleration in x,y,z
-        rpy : roll, pitch, yaw
-
-    Function: removes gravity acceleration from accel values
-
-    Outputs:
-        accel : new accleration in x,y,z
-    '''
-    grav_x = math.sin(math.radians(pitch))
-    grav_y = -math.sin(math.radians(roll))
-    grav_z = -math.cos(math.radians(pitch))*math.cos(math.radians(roll))
-
-    accel_x = accel_x - grav_x
-    accel_y = accel_y - grav_y
-    accel_z = accel_z - grav_z
-
-    return(accel_x, accel_y, accel_z)
->>>>>>> more plotting, created imu.py class
