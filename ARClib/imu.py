3d3b470603349cbdeb5bb4c44c54032a81b2cf72
Thomas Woodruff <jazzyjayhaw@gmail.com>
2020-02-17 15:49:53 -0600

    added heading,gyro readouts; still needs work

--- ARClib/imu.py
+++ ARClib/imu.py
@@ -42,7 +42,7 @@ class IMU:
         elif output == 'quat':
             self.send_command(self.serial_port, 'asc_out,QUATIMU')
         else:
-            raise Exception('Invalid output type. Please use rpy or quat')
+            raise Exception('Invalid output type. Please use \'rpy\' or \'quat\'')
 
         # INITIALIZE VARS
         self.vel_x = 0
@@ -83,10 +83,11 @@ class IMU:
                 # COMPUTE REQ. VARS
                 self._accel_x, self._accel_y, self._accel_z = self.remove_gravity(accel_x, accel_y, accel_z,
                                                                                   pitch, roll, yaw)
-                # self.vel_x = self.vel_x + self._accel_x*9.81*dt
-                # self.vel_y = self.vel_y + self._accel_y*9.81*dt
-                # vel_z = vel_z + accel_z*9.81*dt
-                self.heading = yaw
+
+                self._yaw = yaw
+                self._gyro_x = gyro_x
+                self._gyro_y = gyro_y
+                self._gyro_z = gyro_z
 
             elif(fields[0] == '$QUATIMU'):  #'$QUATIMU'
                 sequence_number, x, y, z, w, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, temperature = (float(x) for x in fields[1:])
@@ -115,7 +116,9 @@ class IMU:
         self.vel_x = self.vel_x + (self._accel_x - self.calib_accel[0])*9.81*self.dt
         self.vel_y = self.vel_y + (self._accel_y - self.calib_accel[1])*9.81*self.dt
 
-        return self.vel_x, self.vel_y, self.heading
+        self.heading = self._yaw - self.heading_ref #self.heading + (self._yaw - self.heading) - self.heading_ref
+
+        return self.vel_x, self.vel_y, self.heading, self._gyro_x, self._gyro_y, self._gyro_z
 
 
     def shutdown(self):
@@ -132,22 +135,25 @@ class IMU:
         acc_x = []
         acc_y = []
         acc_z = []
+        init_time = 3
         start = time.time()
-        while time.time()<(start+3):
+        while time.time()<(start+init_time):
             self.run()
             acc_x.append(self._accel_x)
             acc_y.append(self._accel_y)
             acc_z.append(self._accel_z)
-
         self.calib_accel = [np.mean(acc_x), np.mean(acc_y), np.mean(acc_z)]
+
+        self.run()
+        self.heading_ref = self._yaw
 
     def send_command(self, serial_port, cmd_msg):
         '''
         Inputs:
-            serial_port : range/description
-            cmd_msg : range/description
+            serial_port : serial device to communicate with
+            cmd_msg : data to send to serial_port
 
-        Function: description
+        Function: sends data over serial comms
 
         Outputs:
             line : range/description
@@ -197,31 +203,47 @@ if __name__ == '__main__':
     velx = []
     vely = []
     head = []
-    tim = np.linspace(0, 10, num=100)
+    gyro = []
+    seconds = 3
+    tim = np.linspace(0, seconds, num=10*seconds)
     for i in (tim):
         imu.run()
-        vel_x, vel_y, heading = imu.update()
+        vel_x, vel_y, heading, gyrox, gyroy, gyroz = imu.update()
         velx.append(vel_x)
         vely.append(vel_y)
         head.append(heading)
-        print("x-velocity: {}\ny-velocity: {}\nheading: {}\n".format(vel_x, vel_y, heading))
+        gyro.append([gyrox, gyroy, gyroz])
+
+        #print("x-velocity: {}\ny-velocity: {}\nheading: {}\n".format(vel_x, vel_y, heading))
+        #print("gyro x: {}\ngyro y: {}\ngyro z: {}\n".format(gyrox, gyroy, gyroz))
         time.sleep(0.1)
 
-    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+    gyro = np.array(gyro)
+
+    #plotting results
+    fig1, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex='col')
     ax1.plot(tim, velx, label='x-velocity')
     ax1.plot(tim, vely, label='y-velocity')
-    ax1.legend()
     ax1.set_xlabel('time (s)')
     ax1.set_ylabel('velocity (m/s)')
+    ax1.legend()
     ax1.axis('on')
-    ax2.plot(tim, head)
-    #ax2.plot(timet, y_accel, label='y-acceleration')
-    ax2.set_xlabel('time (s)')
-    ax2.set_ylabel('heading angle (deg)')
-    ax2.legend()
-    #plt.plot(loop_time, vel_z)
-    plt.show()
 
-    #plt.plot(tim, velx)
-    #plt.plot(tim, vely)
-    #plt.show()
+    #ax2.plot(tim, x_accel, label='x-acceleration')
+    #ax2.plot(tim, y_accel, label='y-acceleration')
+    #ax2.set_ylabel('acceleration (m/s^2)')
+    #ax2.legend()
+
+    fig2, (ax3, ax4) = plt.subplots(nrows=2, ncols=1, sharex='col')
+    ax3.plot(tim, gyro[:,0], label='theta_x dot')
+    ax3.plot(tim, gyro[:,1], label='theta_y dot')
+    ax3.plot(tim, gyro[:,2], label='theta_z dot')
+    ax3.set_ylabel('angular velocity (deg/s)')
+    ax3.legend()
+
+    ax4.plot(tim, head)
+    ax4.set_xlabel('time (s)')
+    ax4.set_ylabel('angle (deg)')
+    ax4.legend()
+
+    plt.show()
